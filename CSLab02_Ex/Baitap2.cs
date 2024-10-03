f452cc23ff1e3ec1a7c5c3c3a3905aece05270db
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-04 00:29:38 +0700

    Bổ sung thêm đề bài

--- CSLab02_Ex/Baitap2.cs
+++ CSLab02_Ex/Baitap2.cs
@@ -1,3 +1,16 @@
+// Thông tin một mặt hàng bao gồm: Mã mặt hàng (MaMH - số nguyên), tên mặt hàng (TenMH
+// - chuỗi), số lượng (SoLuong - số nguyên), đơn giá (DonGia - số thực).
+//  Khai báo cấu trúc mặt hàng nói trên, bao gồm cả hàm tạo và hàm tính thành tiền.
+//  Viết hàm thêm một mặt hàng vào trong danh sách.
+//  Viết hàm tìm một mặt hàng dựa vào mã mặt hàng, trả về kiểu bool (có hay không).
+//  Viết hàm xuất danh sách các mặt hàng.
+//  viết hàm xóa mặt hàng dựa vào mã mặt hàng.
+//  Viết hàm main thực hiện các chức năng sau:
+// o nhập vào danh sách các mặt hàng. Nhập xong mỗi mặt hàng hỏi người dùng có
+// tiếp tục nhập hay không?
+// o xuất danh sách các mặt hàng.
+// o nhập vào mã mặt hàng, tiến hành tìm kiếm mặt hàng nói trên. Nếu tìm thấy,
+// xóa mặt hàng và xuất danh sách các mặt hàng (sau khi xóa).
 using System;
 using System.Text;
 using System.Collections.Generic;
