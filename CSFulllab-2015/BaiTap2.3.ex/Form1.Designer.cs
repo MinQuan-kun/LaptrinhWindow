76c9bcb2d857d8bfc5e43afcff0b917674864902
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-15 21:28:29 +0700

    cải tiến bài 2.3

---
+++ CSFulllab-2015/BaiTap2.3.ex/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BaiTap2._3.ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(560, 61);
+            this.Input.Margin = new System.Windows.Forms.Padding(5);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(172, 34);
+            this.Input.TabIndex = 0;
+            this.Input.DoubleClick += new System.EventHandler(this.Output_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập vào số nguyên N\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(33, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kết quả đọc số";
+            // 
+            // Output
+            // 
+            this.Output.ForeColor = System.Drawing.Color.Red;
+            this.Output.Location = new System.Drawing.Point(26, 185);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(503, 200);
+            this.Output.TabIndex = 3;
+            this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(161, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Giới hạn tới hàng tỷ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 759);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Chương trình đọc số";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CSFulllab-2015/BaiTap2.3.ex/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap2._3.ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Output_TextChanged(object sender, EventArgs e)
+        {
+            // Lấy chuỗi đầu vào
+            string inputText = Input.Text;
+
+            // Kiểm tra xem chuỗi có phải là số nguyên dương không
+            if (long.TryParse(inputText, out long number) && number >= 0 && number <= 999999999999)
+            {
+                Output.Text = NumberToWords(number);
+            }
+            else
+            {
+                MessageBox.Show("Lỗi! Vui lòng nhập một số nguyên dương hợp lệ.", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Hàm chuyển đổi số thành chữ
+        private string NumberToWords(long number)
+        {
+            if (number == 0) return "Không";
+
+            string[] units = { "", "nghìn", "triệu", "tỷ" };
+            string[] tens = { "", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+            string[] hundreds = { "", "trăm", "mươi" };
+
+            string words = "";
+            int unitIndex = 0;
+
+            while (number > 0)
+            {
+                long part = number % 1000; // Lấy 3 chữ số cuối
+                if (part > 0)
+                {
+                    string partWords = ConvertPartToWords(part, tens, hundreds);
+                    words = partWords + (unitIndex > 0 ? " " + units[unitIndex] : "") + " " + words;
+                }
+                number /= 1000;
+                unitIndex++;
+            }
+
+            return words.Trim();
+        }
+
+        // Hàm chuyển đổi từng phần 3 chữ số thành chữ
+        private string ConvertPartToWords(long part, string[] tens, string[] hundreds)
+        {
+            string words = "";
+
+            int hundred = (int)(part / 100);
+            int ten = (int)((part % 100) / 10);
+            int one = (int)(part % 10);
+
+            // Nếu có chữ số trăm
+            if (hundred > 0)
+            {
+                words += tens[hundred] + " " + hundreds[1] + " "; // "trăm"
+            }
+
+            // Nếu có chữ số mươi
+            if (ten > 0)
+            {
+                if (ten == 1 && one > 0)
+                {
+                    words += "mười " + tens[one] + " "; // "mười một", "mười hai", ...
+                }
+                else if (ten > 1) // Nếu hàng chục lớn hơn 1
+                {
+                    words += tens[ten] + " " + hundreds[2] + " "; // "mươi"
+                    if (one > 0)
+                    {
+                        words += tens[one] + " "; // Thêm chữ số đơn vị nếu có
+                    }
+                }
+            }
+            else if (one > 0) // Nếu không có chữ số mươi mà có chữ số đơn vị
+            {
+                words += tens[one] + " "; // Thêm chữ số
+            }
+
+            // Trim để loại bỏ khoảng trắng thừa
+            return words.Trim();
+        }
+    }
+}
