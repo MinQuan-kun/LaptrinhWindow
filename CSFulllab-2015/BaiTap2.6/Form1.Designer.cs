65f9747a1904a7c2d6c312df6fb21987858b5921
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-17 15:59:48 +0700

    Bài 2.6 Thiết kế giao diện và cài đặt chương trình

---
+++ CSFulllab-2015/BaiTap2.6/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace BaiTap2._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Input2 = new System.Windows.Forms.TextBox();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.Tong = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Cong = new System.Windows.Forms.RadioButton();
+            this.Tru = new System.Windows.Forms.RadioButton();
+            this.Nhan = new System.Windows.Forms.RadioButton();
+            this.Chia = new System.Windows.Forms.RadioButton();
+            this.Txtpheptoan = new System.Windows.Forms.Label();
+            this.Pheptoan = new System.Windows.Forms.Panel();
+            this.Pheptoan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số hạng thứ nhất";
+            // 
+            // Input2
+            // 
+            this.Input2.Location = new System.Drawing.Point(244, 196);
+            this.Input2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Input2.Name = "Input2";
+            this.Input2.Size = new System.Drawing.Size(104, 34);
+            this.Input2.TabIndex = 1;
+            // 
+            // Input1
+            // 
+            this.Input1.Location = new System.Drawing.Point(244, 131);
+            this.Input1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(104, 34);
+            this.Input1.TabIndex = 2;
+            // 
+            // Tong
+            // 
+            this.Tong.Location = new System.Drawing.Point(244, 265);
+            this.Tong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tong.Name = "Tong";
+            this.Tong.Size = new System.Drawing.Size(104, 34);
+            this.Tong.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 196);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Số hạng thứ hai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 265);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tổng";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 306);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 56);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Tính";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 306);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 56);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Cong
+            // 
+            this.Cong.AutoSize = true;
+            this.Cong.Location = new System.Drawing.Point(3, 14);
+            this.Cong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Cong.Name = "Cong";
+            this.Cong.Size = new System.Drawing.Size(78, 31);
+            this.Cong.TabIndex = 8;
+            this.Cong.TabStop = true;
+            this.Cong.Text = "Cộng";
+            this.Cong.UseVisualStyleBackColor = true;
+            // 
+            // Tru
+            // 
+            this.Tru.AutoSize = true;
+            this.Tru.Location = new System.Drawing.Point(4, 63);
+            this.Tru.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tru.Name = "Tru";
+            this.Tru.Size = new System.Drawing.Size(62, 31);
+            this.Tru.TabIndex = 9;
+            this.Tru.TabStop = true;
+            this.Tru.Text = "Trừ";
+            this.Tru.UseVisualStyleBackColor = true;
+            // 
+            // Nhan
+            // 
+            this.Nhan.AutoSize = true;
+            this.Nhan.Location = new System.Drawing.Point(4, 113);
+            this.Nhan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Nhan.Name = "Nhan";
+            this.Nhan.Size = new System.Drawing.Size(77, 31);
+            this.Nhan.TabIndex = 10;
+            this.Nhan.TabStop = true;
+            this.Nhan.Text = "Nhân";
+            this.Nhan.UseVisualStyleBackColor = true;
+            // 
+            // Chia
+            // 
+            this.Chia.AutoSize = true;
+            this.Chia.Location = new System.Drawing.Point(4, 158);
+            this.Chia.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Chia.Name = "Chia";
+            this.Chia.Size = new System.Drawing.Size(71, 31);
+            this.Chia.TabIndex = 11;
+            this.Chia.TabStop = true;
+            this.Chia.Text = "Chia";
+            this.Chia.UseVisualStyleBackColor = true;
+            // 
+            // Txtpheptoan
+            // 
+            this.Txtpheptoan.AutoSize = true;
+            this.Txtpheptoan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Txtpheptoan.Location = new System.Drawing.Point(366, 80);
+            this.Txtpheptoan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Txtpheptoan.Name = "Txtpheptoan";
+            this.Txtpheptoan.Size = new System.Drawing.Size(95, 27);
+            this.Txtpheptoan.TabIndex = 12;
+            this.Txtpheptoan.Text = "Phép toán";
+            // 
+            // Pheptoan
+            // 
+            this.Pheptoan.Controls.Add(this.Cong);
+            this.Pheptoan.Controls.Add(this.Tru);
+            this.Pheptoan.Controls.Add(this.Chia);
+            this.Pheptoan.Controls.Add(this.Nhan);
+            this.Pheptoan.Location = new System.Drawing.Point(387, 110);
+            this.Pheptoan.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Pheptoan.Name = "Pheptoan";
+            this.Pheptoan.Size = new System.Drawing.Size(148, 194);
+            this.Pheptoan.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 433);
+            this.Controls.Add(this.Txtpheptoan);
+            this.Controls.Add(this.Pheptoan);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tong);
+            this.Controls.Add(this.Input1);
+            this.Controls.Add(this.Input2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Chương trình máy đơn giản";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Pheptoan.ResumeLayout(false);
+            this.Pheptoan.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Input2;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.TextBox Tong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton Cong;
+        private System.Windows.Forms.RadioButton Tru;
+        private System.Windows.Forms.RadioButton Nhan;
+        private System.Windows.Forms.RadioButton Chia;
+        private System.Windows.Forms.Label Txtpheptoan;
+        private System.Windows.Forms.Panel Pheptoan;
+    }
+}
+
---
+++ CSFulllab-2015/BaiTap2.6/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap2._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string n1 = Input1.Text;
+            string n2 = Input2.Text;
+            double res = 0;
+            var culture = CultureInfo.InvariantCulture.Clone() as CultureInfo;
+            culture.NumberFormat.NumberDecimalSeparator = ","; // Sử dụng dấu phẩy
+            culture.NumberFormat.NumberGroupSeparator = ".";   // Sử dụng dấu chấm nếu có
+            if (!double.TryParse(n1, NumberStyles.Any, culture, out double result1))
+            {
+                culture.NumberFormat.NumberDecimalSeparator = ".";
+                if (!double.TryParse(n1, NumberStyles.Any, culture, out result1))
+                {
+                    MessageBox.Show("Lỗi! Vui lòng nhập một số thực hợp lệ.", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            if(!double.TryParse(n2, NumberStyles.Any, culture, out double result2))
+            {
+                culture.NumberFormat.NumberDecimalSeparator = ".";
+                if (!double.TryParse(n2, NumberStyles.Any, culture, out result2))
+                {
+                    MessageBox.Show("Lỗi! Vui lòng nhập một số thực hợp lệ.", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            if (Cong.Checked == true)
+                res = result1 + result2;
+            else if (Tru.Checked == true)
+                res = result1 - result2;
+            else if (Nhan.Checked == true)
+                res = result1 * result2;
+            else if (Chia.Checked == true)
+            {
+                if (result2 == 0)
+                {
+                    MessageBox.Show("Lỗi! Không thể chia cho 0.", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                res = result1 / result2;
+            }
+            // Display the result
+            Tong.Text = res.ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           DialogResult result = MessageBox.Show(
+               "Bạn có chắc chắn thoát", 
+               "Confirm",
+               MessageBoxButtons.YesNo, 
+               MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+                Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cong.Checked = true;
+        }
+    }
+}
